07b2e46a28abff936c9c6a312e2b5a8ea9355f6e
Asilbek-Sodiqov <asilbeksodiqov629@gmail.com>
2024-05-17 01:00:02 +0500

    Add all

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 var elMenuBtn = document.querySelector(".js-menu-btn");
 var elSiteHeader = document.querySelector(".site-header");
+var elHeaderTheme = document.querySelector(".js-dark");
 elMenuBtn.addEventListener("click", function(){
   elSiteHeader.classList.toggle("open-menu");
   document.body.classList.toggle("unscrollbody");
+});
+elHeaderTheme.addEventListener("click", function () {
+  document.body.classList.toggle("dark-theme");
 });
